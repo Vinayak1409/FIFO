eaf3238277de8cc22e14c350787566adbcd6cc4b
Vinayak <tulsyanvinayak@gmail.com>
2023-07-14 10:07:45 +0530

    added comments

--- FIFO.v
+++ FIFO.v
@@ -1,32 +1,32 @@
 module FIFO(clk,rst, buf_in, buf_out, wr_en, rd_en, buf_empty, buf_full, fifo_counter);
 
 input rst, clk, wr_en, rd_en;
-input [7:0] buf_in;
-output [7:0]buf_out;
-output buf_empty, buf_full;
-output [7:0] fifo_counter;
+input [7:0] buf_in; // data that will be written in the buffer
+output [7:0]buf_out; // data that will be read out from FIFO
+output buf_empty, buf_full; // Flag to indicate if the FIFO is empty or full
+output [7:0] fifo_counter; // counting the location in buffer
 
 reg[7:0] buf_out;
 reg buf_empty, buf_full;
 reg[6:0] fifo_counter;
-reg[3:0] rd_ptr, wr_ptr;
-reg[7:0] buf_mem[63:0];
+reg[3:0] rd_ptr, wr_ptr; // pointer that will move back and forth within the FIFO
+reg[7:0] buf_mem[63:0]; // total memory location
 
 always@(fifo_counter) 
 begin
-  buf_empty=(fifo_counter==0);
-  buf_full = (fifo_counter == 64);
+  buf_empty=(fifo_counter==0); //when fifo_counter is 0 flag goes up
+  buf_full = (fifo_counter == 64); //when fifo_counter is 64 flag goes up
 end
 
 always@(posedge clk or posedge rst) 
 begin
     if(rst)
         fifo_counter <= 0;
-    else if((!buf_full && wr_en) && (!buf_empty && rd_en))
+    else if((!buf_full && wr_en) && (!buf_empty && rd_en)) 
         fifo_counter <= fifo_counter;
-    else if(!buf_full && wr_en)
+    else if(!buf_full && wr_en) // when FIFO is not full and wr_en is HIGH increment the counter
         fifo_counter <= fifo_counter+1;
-    else if(!buf_empty && rd_en)
+    else if(!buf_empty && rd_en) // when FIFO is not empty and rd_en is HIGH decrement the counter
         fifo_counter <= fifo_counter-1;
     else 
         fifo_counter <= fifo_counter;
@@ -39,7 +39,7 @@ begin
     else 
     begin
         if(rd_en && !buf_empty)
-            buf_out <= buf_mem[rd_ptr];
+            buf_out <= buf_mem[rd_ptr]; //reading from the FIFO
         else 
             buf_out <= buf_out;
     end
@@ -48,12 +48,12 @@ end
 always@(posedge clk)
 begin
       if(wr_en && !buf_full)
-        buf_mem[wr_ptr] <= buf_in;
+        buf_mem[wr_ptr] <= buf_in; // writing into the FIFO
       else 
         buf_mem[wr_ptr] <= buf_mem[wr_ptr];
 end
 
-always@(posedge clk or posedge rst)
+always@(posedge clk or posedge rst) // manipulating pointers when read and write flags are up concurrently
 begin
     if(rst)
     begin
