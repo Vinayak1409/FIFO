1b7625b1be9825b49563f047cc8d8cf20f025e10
Vinayak <tulsyanvinayak@gmail.com>
2023-07-15 09:37:08 +0530

    commit

--- FIFO_tb.v
+++ FIFO_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 14.07.2023 10:10:38
-// Design Name: 
-// Module Name: FIFO_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module FIFO_tb(
 
